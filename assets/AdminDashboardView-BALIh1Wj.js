b43deef9a13acb76e3b5da5c9c933a96885079d7
parsgit <nvioir@gmail.com>
2025-03-19 16:02:00 +0330

    update server

--- assets/AdminDashboardView-BALIh1Wj.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index-D6Ucjarz.js";const e={};function o(a,c){return"Hello Dashboard"}const s=r(e,[["render",o]]);export{s as default};
---
+++ assets/AdminDashboardView-CPzGV_dE.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index-8JBQnCp2.js";const e={};function o(a,c){return"Hello Dashboard"}const s=r(e,[["render",o]]);export{s as default};
